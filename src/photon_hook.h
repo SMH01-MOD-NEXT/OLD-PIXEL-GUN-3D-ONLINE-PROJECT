c5d9e479d9082a7c13ddb7f6d07ee4ad1767e21c
SMH01 <smali.pg@outlook.com>
2026-08-19 21:22:03 +0300

    Gradle rework: drop old src/photon_hook.h (moved into opg3d module)

--- src/photon_hook.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-namespace photon {
-
-// Ставит хук на PhotonNetwork.ConnectUsingSettings (il2cpp_base + RVA из дампа).
-// true = хук установлен.
-bool install_hook(void* il2cpp_base);
-
-} // namespace photon
